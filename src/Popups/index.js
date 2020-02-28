37f622dfb17f92b47ae9561a636c6a8b741b298d
Jaime Scowcroft <jaime.scowcroft@stockport.gov.uk>
2020-02-28 08:36:59 +0000

    first commit

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -1,47 +1,20 @@
-const tpoPopup = (feature, layer) => {
-  
-  const content = `<div class="item"><i class="fa fa-tree" aria-hidden="true"></i><p class="title">Tree Preservation Order</p>
-  <p></p>
-  <p class="info">Reference: ${feature.properties.tpo_number}</p>
-  <p class="info">Tree Number: ${feature.properties.tree_number}</p>
-  <p class="info">Name: ${feature.properties.tpo_name}</p>
-  <p class="info">Species: ${feature.properties.tree_species}</p>
-  <p class="info">Status: ${feature.properties.status}</p>
-  
-  </div>`
- 
+const Land_Ownership_Popup = (feature, layer) => {
+  const content = `<div class="item"><i class="fa fa-tree" aria-hidden="true"></i><p class="title">Land Ownership</p>
+    <p></p>
+    <p class="info">Comittee: ${feature.properties.committee_new}</p>
+    <p class="info">Title Number: ${feature.properties.titlenumber}</p>
+    </div>`
   layer.bindPopup(content)
- }
-
- const conservationPopup = (feature, layer) => {
-  
-  const content = `<div class="item"><i class="fa fa-home" aria-hidden="true"></i><p class="title">Conservation Area</p>
-  <p></p>
-  <p class="info">Name: ${feature.properties.cons_area}</p>
-  
-  </div>`
- 
+}
+const Leases_Popup = (feature, layer) => {
+  const content = `<div class="item"><i class="fa fa-tree" aria-hidden="true"></i><p class="title">Leases</p>
+    <p></p>
+    <p class="info">Description: ${feature.properties.description}</p>
+    </div>`
   layer.bindPopup(content)
- }
-
- const planappPopup = (feature, layer) => {
-  
-  const content = `<div class="item"><i class="fa fa-map-o" aria-hidden="true"></i><p class="title">Tree Planning App</p>
-  <p></p>
-  <p class="info">Reference: ${feature.properties.refval}</p>
-  <p class="info">Proposal: ${feature.properties.proposal}</p>
-  <p class="info">Address: ${feature.properties.address}</p>
-  <p class="info">Received Date: ${feature.properties.received_date}</p>
-  <p class="info">Decision: ${feature.properties.decision}</p>
-  <p class="info">Decision Date: ${feature.properties.decision_date}</p>
-  
-  </div>`
- 
-  layer.bindPopup(content)
- }
+}
 
 export {
-  tpoPopup,
-  conservationPopup,
-  planappPopup
+  Land_Ownership_Popup,
+  Leases_Popup
 }
